eeba9e0fa6dad8689c4d79c92b57b2434f58e343
Talal146 <talal.abdullah146@gmail.com>
2024-07-06 18:22:13 +0300

    cat ctrl

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -1,1 +1,14 @@
+const { Category } = require('../models');
 
+const getAllCategories = async (req, res) => {
+	try {
+		let categories = await Category.find({});
+		res.send(categories);
+	} catch (error) {
+		console.error('Error fetching categories', erorr);
+	}
+};
+
+module.exports = {
+  getAllCategories
+};
